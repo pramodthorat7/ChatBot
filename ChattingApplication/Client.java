7f4d75c381b9ea095f8336675dabbb1444707a61
Pramod Thorat <pramodthorat410@gmail.com>
2025-06-13 12:08:40 +0530

    `Refactored Server class: removed redundant code, updated layout, and added functionality for sending messages and formatting labels.`

--- ChattingApplication/Client.java
+++ ChattingApplication/Client.java
@@ -1,3 +1,4 @@
+
 import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.*;
@@ -7,15 +8,15 @@ import java.text.*;
 import java.net.*;
 import java.io.*;
 
-public class Server implements ActionListener {
+public class Client implements ActionListener {
 
-    JTextField text;
-    JPanel a1;
-    static Box vertical = Box.createVerticalBox();
     static JFrame f = new JFrame();
+    JTextField text;
+    static JPanel a1;
+    static Box vertical = Box.createVerticalBox();
     static DataOutputStream dout;
 
-    Server() {
+    Client() {
 
         f.setLayout(null);
 
@@ -38,35 +39,35 @@ public class Server implements ActionListener {
             }
         });
 
-        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/1.png"));
-        Image i5 = i4.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/2.png"));
+        Image i5 = i4.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
         ImageIcon i6 = new ImageIcon(i5);
         JLabel profile = new JLabel(i6);
         profile.setBounds(40, 10, 50, 50);
         p1.add(profile);
 
         ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icons/video.png"));
-        Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+        Image i8 = i7.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
         ImageIcon i9 = new ImageIcon(i8);
         JLabel video = new JLabel(i9);
         video.setBounds(300, 20, 30, 30);
         p1.add(video);
 
         ImageIcon i10 = new ImageIcon(ClassLoader.getSystemResource("icons/phone.png"));
-        Image i11 = i10.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
+        Image i11 = i10.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
         ImageIcon i12 = new ImageIcon(i11);
         JLabel phone = new JLabel(i12);
         phone.setBounds(360, 20, 35, 30);
         p1.add(phone);
 
         ImageIcon i13 = new ImageIcon(ClassLoader.getSystemResource("icons/3icon.png"));
-        Image i14 = i13.getImage().getScaledInstance(10, 25, Image.SCALE_DEFAULT);
+        Image i14 = i13.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
         ImageIcon i15 = new ImageIcon(i14);
         JLabel morevert = new JLabel(i15);
-        morevert.setBounds(420, 20, 10, 25);
+        morevert.setBounds(410, 20, 10, 25);
         p1.add(morevert);
 
-        JLabel name = new JLabel("UserOne");
+        JLabel name = new JLabel("User Two");
         name.setBounds(110, 15, 100, 18);
         name.setForeground(Color.WHITE);
         name.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
@@ -79,7 +80,7 @@ public class Server implements ActionListener {
         p1.add(status);
 
         a1 = new JPanel();
-        a1.setBounds(5, 75, 440, 570);
+        a1.setBounds(5, 70, 440, 570);
         f.add(a1);
 
         text = new JTextField();
@@ -91,21 +92,24 @@ public class Server implements ActionListener {
         send.setBounds(320, 655, 123, 40);
         send.setBackground(new Color(7, 94, 84));
         send.setForeground(Color.WHITE);
-        send.addActionListener(this);
-        send.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        send.addActionListener(this); // Action performed
+        send.setFont(new Font("SAN_SERIF", Font.BOLD, 16));
         f.add(send);
 
         f.setSize(450, 700);
-        f.setLocation(200, 30);
+        f.setLocation(700, 20);
         f.setUndecorated(true);
         f.getContentPane().setBackground(Color.WHITE);
 
         f.setVisible(true);
     }
 
+    // @Override
     public void actionPerformed(ActionEvent ae) {
+        // Handle action events here
         try {
             String out = text.getText();
+            a1.setLayout(new BorderLayout());
 
             JPanel p2 = formatLabel(out);
 
@@ -116,15 +120,15 @@ public class Server implements ActionListener {
             vertical.add(right);
             vertical.add(Box.createVerticalStrut(15));
 
+            text.setText("");
             a1.add(vertical, BorderLayout.PAGE_START);
 
             dout.writeUTF(out);
 
-            text.setText("");
-
             f.repaint();
             f.invalidate();
             f.validate();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -134,7 +138,7 @@ public class Server implements ActionListener {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        JLabel output = new JLabel("<html><p style=\"width: 150px\">" + out + "</p></html>");
+        JLabel output = new JLabel("<html><p style = \"width: 150px\">" + out + "</p></html>");
         output.setFont(new Font("Tahoma", Font.PLAIN, 16));
         output.setBackground(new Color(37, 211, 102));
         output.setOpaque(true);
@@ -147,34 +151,38 @@ public class Server implements ActionListener {
 
         JLabel time = new JLabel();
         time.setText(sdf.format(cal.getTime()));
-
         panel.add(time);
 
         return panel;
     }
 
     public static void main(String[] args) {
-        new Server();
+
+        new Client();
 
         try {
-            ServerSocket skt = new ServerSocket(6001);
+            Socket s = new Socket("127.0.0.1", 6001);
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            dout = new DataOutputStream(s.getOutputStream());
+
             while (true) {
-                Socket s = skt.accept();
-                DataInputStream din = new DataInputStream(s.getInputStream());
-                dout = new DataOutputStream(s.getOutputStream());
+                a1.setLayout(new BorderLayout());
+                String msg = din.readUTF();
+                JPanel panel = formatLabel(msg);
 
-                while (true) {
-                    String msg = din.readUTF();
-                    JPanel panel = formatLabel(msg);
+                JPanel left = new JPanel(new BorderLayout());
+                left.add(panel, BorderLayout.LINE_START);
+                vertical.add(left);
 
-                    JPanel left = new JPanel(new BorderLayout());
-                    left.add(panel, BorderLayout.LINE_START);
-                    vertical.add(left);
-                    f.validate();
-                }
+                vertical.add(Box.createVerticalStrut(15));
+                a1.add(vertical, BorderLayout.PAGE_START);
+
+                f.validate();
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 }
